17879e7ee65aab5261e52af55521f0b7ac1303b6
PeCoBe <pedro.cortinab@gmail.com>
2022-05-25 16:15:46 -0500

    Implemented Block

---
+++ src/main.rs
@@ -0,0 +1,11 @@
+use blockchainlib::*;
+
+fn main () {
+    let block = Block::new(0, 0, vec![0; 32], 0, "Genesis block".to_owned());
+
+    println!("{:?}", &block);
+
+    let h = block.hash();
+
+    println!("{:?}", &h);
+}
